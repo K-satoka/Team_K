b8e9bebe207f2b229a330cf0ce12c40e17f8e2ab
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-10-20 14:51:02 +0900

    移動プログラム

---
+++ Team_K/Assets/ioka yuzuki/Player/PlaerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlaerController : MonoBehaviour
+{
+    Rigidbody2D rbody;  // Rigidbody2D型の変数
+    float axisH = 0.0f; //入力
+    public float speed = 805.0f;
+
+    public float jump=9.0f;
+    public LayerMask groundLayer;
+    bool goJump =false;
+
+
+    void Start()
+    {
+        // Rigidbody2Dをとってくる
+        rbody=this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        //水平方向をチェックする
+        axisH = Input.GetAxisRaw("Horizontal");
+        if(axisH>0.0f)
+        {
+            //Debug.Log("右移動");
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if(axisH<0.0f)
+        {
+            Debug.Log("左移動");
+            transform.localScale=new Vector2(1, 1);
+        }
+        //キャラクターをジャンプさせる
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //地上判定
+        bool onGround = Physics2D.CircleCast(transform.position,//発射位置
+            0.2f,            //円の半径
+            Vector2.down,    //発射方向
+            0.0f,            //発射距離
+            groundLayer);    //検出するレイヤー
+        if (onGround || axisH != 0)
+        {
+            //地面の上or速度が０ではない
+            //速度を更新する
+            rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
+        }
+        if (onGround) Debug.Log("tettse");
+        if (onGround && goJump) {
+            //地面の上でジャンプキーが押された
+            //ジャンプさせる
+            Vector2 jumpPw = new Vector2(0, jump);//ジャンプさせりベクトルを作る
+           rbody.AddForce(jumpPw, ForceMode2D.Impulse);//
+            goJump = false;
+            Debug.Log("ジャンプ");
+        }
+       
+    }
+ //ジャンプ
+ public void Jump()
+    {
+        goJump = true;//ジャンプフラグを立てる
+    }
+}
---
+++ Team_K/Assets/ioka yuzuki/Player/Player_Move_Transition.cs
@@ -0,0 +1,42 @@
+﻿//using JetBrains.Annotations;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Player_Move_Transition : MonoBehaviour
+//{
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //GetComponentを用いてAnimatorコンポーネントを取り出す
+//        Animator animator = GetComponent<Animator>();
+
+//        //あらかじめ設定していたintパラメータ「Player_move_trans」の値を取り出す
+//        int Player_move_trans = animator.GetInteger("Player_move_trans");
+       
+//        //左右の矢印キーを押した際にパラメータ「Player_move_trans」の値を増加させる
+//        if (Input.GetKeyDown(KeyCode.RightArrow))
+//        {
+//            Player_move_trans++;
+//            if (Player_move_trans >= 2)
+//            {
+//                Player_move_trans--;
+//            }
+
+//            Debug.Log(Player_move_trans);
+//        }
+//        if (Input.GetKeyDown(KeyCode.LeftArrow))
+//        {
+//            Player_move_trans++;
+//            if (Player_move_trans >= 2)
+//            {
+//                Player_move_trans--;
+//            }
+
+//            Debug.Log(Player_move_trans);
+//        }
+//        //intパラメーターの値を設定する.
+//        animator.SetInteger("Player_move_trans", Player_move_trans);
+
+//    }
+//}
