56b30a3f128a4cbaac34b6ad57218294b1888575
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2025-11-05 12:10:59 +0900

    ステージセレクト途中まで

---
+++ Team_K/Assets/masuda haruto/StageSelect/Script/NextStage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextStage : MonoBehaviour
+{
+    //ボタンを配列として定義
+    [SerializeField]
+    private
+        Button[] _stageButton;
+    void Start()
+    {
+        //ステージのCLEAR数を取得
+        int stageUnlock = PlayerPrefs.GetInt("StageUnlock", 1);
+
+        //ステージボタンの表示・非表示
+        for(int i=0;i < _stageButton.Length;i++)
+        {
+            if (i < stageUnlock)
+            {
+                _stageButton[i].interactable = true;
+                Debug.Log("はい！！！！");
+            }
+            else
+                _stageButton[i].interactable = false;
+        }
+    }
+
+    public void StageSelect(int stage)
+    {
+        //受け取った因数(stage)のステージをロード
+        SceneManager.LoadScene(stage);
+    }
+}
