75fdddde1778c4979f95c305eb7683c7e0255eb2
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2025-11-06 11:44:09 +0900

    あ

--- Team_K/Assets/ioka yuzuki/Player/PlayerHP.cs
+++ Team_K/Assets/ioka yuzuki/Player/PlayerHP.cs
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
+﻿
 ﻿using Unity.VisualScripting;
-=======
+
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
->>>>>>> 0ddffe4b332d391624d52345ad0ea1ea5bc59a64
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -27,7 +25,7 @@ public class PlayerHP : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
+
         Player_Current_Hp = Player_MAX_Hp;
 
         // スライダー初期化
@@ -36,9 +34,9 @@ public class PlayerHP : MonoBehaviour
             PlayerhpSlider.maxValue = Player_MAX_Hp;
             PlayerhpSlider.value = Player_Current_Hp;
         }
-=======
+
      rbody = GetComponent<Rigidbody2D>();
->>>>>>> 0ddffe4b332d391624d52345ad0ea1ea5bc59a64
+
     }
 
     // Update is called once per frame
@@ -67,24 +65,7 @@ public class PlayerHP : MonoBehaviour
        
     }
 
-<<<<<<< HEAD
-    
-=======
-    //攻撃を受けたときに呼ぶ
-   
 
-    //public void TakeDamage(int damage)
-    //{
-    //    Player_Current_Hp -= damage;
-    //    if (Player_Current_Hp < 0) Player_Current_Hp = 0;//HPがマイナスにならないように
-
-    //    if (Player_Slider != null)
-    //    {
-    //        Player_Slider.value = Player_Current_Hp;//HPバー更新
-    //    }
-
-    //}
->>>>>>> 0ddffe4b332d391624d52345ad0ea1ea5bc59a64
     void death()
     {
         // シーンを切り替える
