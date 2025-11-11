1d5b2b023768f46f8e6241e846ec13de2c3b55e7
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2025-11-11 11:27:30 +0900

    げーむおーば画面(リトライはまだ )

---
+++ Team_K/Assets/ioka yuzuki/GameOver/SelectTrans.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectTrans : MonoBehaviour
+{
+    public void LoadScene(string StageSelect)
+    {
+        SceneManager.LoadScene(StageSelect);
+    }
+}
