7304da958abeecab724bdeaf5077b911458ae71b
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-11-17 16:21:25 +0900

    bossatk

--- Team_K/Assets/ioka yuzuki/lastboss/lastboss.cs
+++ Team_K/Assets/ioka yuzuki/lastboss/lastboss.cs
@@ -14,6 +14,12 @@ public class lastboss : MonoBehaviour
     public float teleportInterval = 3f; // 何秒ごとにテレポートするか
     public Vector2 teleportRange = new Vector2(100f, 50f); // テレポート範囲
 
+    public float firespeed = 10;
+    public float fireInterval = 1.5f;
+    public GameObject firePrefab;
+    public Transform firepoint;
+    private float timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,7 +29,31 @@ public class lastboss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer>=fireInterval)
+        {
+            fire();
+            timer = 0;
+        }
+    }
+
+    void fire()
+    {
+        if (firepoint == null)
+        {
+            Debug.LogError("FirePoint がシーン上で null です！ どの lastboss が null か確認してください。", this);
+            return;
+        }
+        GameObject fire = Instantiate(firePrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rbody=fire.GetComponent<Rigidbody2D>();
+        rbody.linearVelocity = firepoint.right * firespeed;
+
+    }
+    void thunder()
+    {
+
+
+
     }
 
     void Teleport()
