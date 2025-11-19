593e9f1a04ac70c423fa7048a033361e75459f5f
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-11-19 16:31:50 +0900

    ラスボス攻撃テレポート

--- Team_K/Assets/ioka yuzuki/lastboss/fire.cs
+++ Team_K/Assets/ioka yuzuki/lastboss/fire.cs
@@ -1,24 +1,38 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class firebullet : MonoBehaviour
 {
     private Rigidbody2D rb;
 
     public float speed = 50.0f;
-    public float lifetime = 30f;
+    public float lifetime = 15f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = -transform.right * speed;
-        Destroy(gameObject, lifetime);//éûä‘åoâﬂÇ≈ã Ç™è¡Ç¶ÇÈ
+        if(transform.position.x>0)
+        {
+            rb.linearVelocity = -transform.right * speed;
+        }
+        else
+        {
+            rb.linearVelocity = transform.right * speed;
+            Vector2 scale = transform.localScale;
+            scale.x *= -1; // X ÇîΩì]
+            transform.localScale = scale;
+        }
+
+            Destroy(gameObject, lifetime);//éûä‘åoâﬂÇ≈ã Ç™è¡Ç¶ÇÈ
     }
 
     // Update is called once per frame
     void Update()
     {
+    
     }
 
 
