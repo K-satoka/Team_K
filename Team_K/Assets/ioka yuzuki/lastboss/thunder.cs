2aed46936d87bd761e20a2cb6f01097cc0da48f8
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-12-01 11:59:24 +0900

    雷また今度

---
+++ Team_K/Assets/ioka yuzuki/lastboss/thunder.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class thunder : MonoBehaviour
+{
+    public GameObject thunderlanding_Prefab;
+    public Transform thunderlanding_Point;
+
+    private Rigidbody2D rb;
+    public float landing_speed;
+    public float lifetime = 15f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Ground")
+        {
+            ThunderlandingShot();
+            
+        }
+
+    }
+
+    void ThunderlandingShot()
+    {
+        Debug.Log("‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ");
+        Instantiate(thunderlanding_Prefab, thunderlanding_Point.position, thunderlanding_Point.rotation);
+    }
+}
