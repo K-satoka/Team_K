0ed5c1572f8de370fcf83392c2d8f1592114d989
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2025-12-01 14:41:37 +0900

    ああああああああああ

--- Team_K/Assets/masuda haruto/Ending/Ending.cs
+++ Team_K/Assets/masuda haruto/Ending/Ending.cs
@@ -1,54 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Rendering;
 
-public class Ending:MonoBehaviour
+public class Ending : MonoBehaviour
 {
-    //テキストのスクロールスピード
-    [SerializeField]
-    private float textScrollSpeed = 30;
-    //テキストの制限位置
-    [SerializeField]
-    private float limitPosition = 730f;
-    //エンドロールが終了したかどうか
-    private bool isStopEndRoll;
-    //　シーン移動用コルーチン
-    private Coroutine endRollCoroutine;
-    void Update()
+    [SerializeField] private GameObject confirmPanel;
+
+    // ボタンを押したときに呼ぶ
+    public void OnClickButton()
     {
-        //　エンドロールが終了した時
-        if (isStopEndRoll)
-        {
-            endRollCoroutine = StartCoroutine(GoToNextScene());
-        }
-        else
-        {
-            //　エンドロール用テキストがリミットを越えるまで動かす
-            if (transform.position.y <= limitPosition)
-            {
-                transform.position = new Vector2(transform.position.x, transform.position.y + textScrollSpeed * Time.deltaTime);
-            }
-            else
-            {
-                isStopEndRoll = true;
-            }
-        }
+        confirmPanel.SetActive(true);   // 確認パネルを表示
     }
 
-    IEnumerator GoToNextScene()
+    // OKボタン
+    public void OnClickOK()
     {
-        //　5秒間待つ
-        yield return new WaitForSeconds(5f);
+        Debug.Log("実行しました");
+        confirmPanel.SetActive(false);
+        FadeManager.Instance.LoadScene("Title", 1.0f);
+    }
 
-        if (Input.GetKeyDown("space"))
-        {
-            StopCoroutine(endRollCoroutine);
-            SceneManager.LoadScene("StageSelect");
-        }
-
-        yield return null;
+    // キャンセルボタン
+    public void OnClickCancel()
+    {
+        confirmPanel.SetActive(false);
     }
 }
