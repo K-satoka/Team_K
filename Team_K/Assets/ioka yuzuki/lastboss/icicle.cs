5e6232e729574efc434a828860a0aa1421a14610
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-12-10 16:31:07 +0900

    jditgtdiu

--- Team_K/Assets/ioka yuzuki/lastboss/icicle.cs
+++ Team_K/Assets/ioka yuzuki/lastboss/icicle.cs
@@ -4,7 +4,7 @@ public class icicle : MonoBehaviour
 {
     public int ice_damage;
 
-
+    Animator anim;
     private Rigidbody2D rb;
     public float landing_speed;
     public float lifetime = 15f;
@@ -16,25 +16,29 @@ public class icicle : MonoBehaviour
     }
     private void Update()
     {
-
+        anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            anim.Play("break");
             Destroy(gameObject);
         }
         else if (collision.gameObject.tag == "Ground")
         {
+            anim.Play("break");
             Destroy(gameObject);
         }
         else if(collision.gameObject.tag =="Enemy")
         {
+            anim.Play("break");
             Destroy(gameObject);
         }
         else if(collision.gameObject.tag=="BossATK")
         {
+            anim.Play("break");
             Destroy(gameObject);
         }
 
