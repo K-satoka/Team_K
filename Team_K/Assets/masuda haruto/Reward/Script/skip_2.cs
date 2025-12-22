1e4f24733fbc4c8c92aacb3d165b37d9ca608a96
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2025-12-22 09:43:22 +0900

    dfaf

---
+++ Team_K/Assets/masuda haruto/Reward/Script/skip_2.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン管理に必要
+
+public class skip_2 : MonoBehaviour
+{
+
+    // ボタンが押されたら呼ばれる関数
+
+    private bool isClicked = false; // ボタンが押されたかどうか
+
+    public void ChangeScene()
+    {
+        if (isClicked) return; // すでに押されていたら何もしない
+
+        isClicked = true; // 一度押されたことを記録
+        FadeManager.Instance.LoadScene("StageSelect", 1.0f);
+
+    }
+}
