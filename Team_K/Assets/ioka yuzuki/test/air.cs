11f76a0bcf4a4f8351bdf40543156248711e69e2
ioka yuzuki <h1202515018@hiratagakuen.onmicrosoft.com>
2026-01-19 14:02:58 +0900

    あ

--- Team_K/Assets/ioka yuzuki/test/air.cs
+++ Team_K/Assets/ioka yuzuki/test/air.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 public class air : MonoBehaviour
 {
     Camera cam;
-    float r = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         cam = GetComponent<Camera>();
+
+        // îOÇÃÇΩÇﬂèâä˙âªÅi255,255,255Åj
+        cam.backgroundColor = Color.white;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Color c = cam.backgroundColor;
 
-        c.r += Time.deltaTime * 0.2f;
-        c.b -= Time.deltaTime * 0.2f;
+        // g Ç∆ b Çè≠ÇµÇ∏Ç¬å∏ÇÁÇ∑
+        c.g -= Time.deltaTime * 0.1f;
+        c.b -= Time.deltaTime * 0.1f;
 
-        c.r = Mathf.Clamp01(c.r);
+        c.g = Mathf.Clamp01(c.g);
         c.b = Mathf.Clamp01(c.b);
 
         cam.backgroundColor = c;
