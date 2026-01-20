4e78c4b4ebb8ff450afb1e9f1d1436bb890f3e6f
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2026-01-20 12:29:58 +0900

    aaaaaaaaa

--- Team_K/Assets/masuda haruto/Ending/Ending.cs
+++ Team_K/Assets/masuda haruto/Ending/Ending.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Ending : MonoBehaviour
 {
     [SerializeField] private GameObject confirmPanel;
+    [SerializeField] private Button okButton;
 
+    [SerializeField] private CanvasGroup mainCanvasGroup;
+    [SerializeField] private CanvasGroup confirmCanvasGroup;
 
     public AudioSource audioSource;
     public AudioClip EndingSE;
@@ -11,23 +17,49 @@ public class Ending : MonoBehaviour
     public void OnClickButton()
     {
         confirmPanel.SetActive(true);   // 確認パネルを表示
+        StartCoroutine(SelectOKNextFrame());
+
+        //裏側の入力を停止
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        //確認用のパネルを有効か
+        confirmCanvasGroup.interactable = true;
+        confirmCanvasGroup.blocksRaycasts = true;
+
+     
     }
 
     // OKボタン
     public void OnClickOK()
     {
-        Debug.Log("実行しました");
-        confirmPanel.SetActive(false);
-        if (audioSource != null && EndingSE != null)
-            audioSource.PlayOneShot(EndingSE);
+        CloseConfirm();
         FadeManager.Instance.LoadScene("Title", 1.0f);
     }
 
     // キャンセルボタン
     public void OnClickCancel()
     {
+        CloseConfirm();
+    }
+
+    void CloseConfirm()
+    {
+        confirmPanel.SetActive(false);
+
+        //裏側の復活
+        mainCanvasGroup.interactable = true;
+        mainCanvasGroup.blocksRaycasts = true;
+
         if (audioSource != null && EndingSE != null)
             audioSource.PlayOneShot(EndingSE);
-        confirmPanel.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    private IEnumerator SelectOKNextFrame()
+    {
+        yield return null;
+        EventSystem.current.SetSelectedGameObject(okButton.gameObject);
     }
 }
