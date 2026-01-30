1b56cab8fad1cc93859cefb94d3dfa2fae80f349
Masuda Haruto <h1202516005@hiratagakuen.onmicrosoft.com>
2026-01-30 09:23:00 +0900

    チュートリアル効果音

--- Team_K/Assets/masuda haruto/script/Tutorial.cs
+++ Team_K/Assets/masuda haruto/script/Tutorial.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Tutorial : MonoBehaviour
 {
+
+    //SE
+    public AudioSource audioSource;
+    public AudioClip TutorialSE;
     //エンターが押されたらシーン移行する
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
+            if (audioSource != null && TutorialSE)
+                audioSource.PlayOneShot(TutorialSE);
+            
             FadeManager.Instance.LoadScene("StageSelect", 1.0f);
         }
     }
